23d7f2ff1c0019a3906398a1680949e16c188c57
MikeLampkin <mike@lampkin.net>
2012-12-21 16:40:10 -0600

    Updated user model and views, added more graphics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_confirmation
+  attr_accessible :active, :admin, :email, :password, :password_confirmation
+  
+  before_save :first_is_admin
+  
+  has_secure_password
+  
+  private
+  
+  def first_is_admin
+     if User.count < 1
+      self.admin = true
+      self.active = true
+    end
+  end
 end

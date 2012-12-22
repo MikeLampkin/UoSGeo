88bb5d6c514cc7d01b768053a2a193a346954384
MikeLampkin <mike@lampkin.net>
2012-12-22 15:27:05 -0600

    Updates but i don't know what I'm doing.

---
+++ app/controllers/report_controller.rb
@@ -0,0 +1,24 @@
+
+class ReportController < ApplicationController
+
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+    @post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @post }
+    end
+  end
+
+end
